0d7c90d8408c4c81cab6fdeae27e4a3eba43ce92
Xiang <iasonliu@gmail.com>
2013-12-31 04:51:40 +0800

    Added enough posts that pagination is necessary

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,4 +1,3 @@
-
 if (Posts.find().count() === 0) {
   var now = new Date().getTime();
 var tomId = Meteor.users.insert({
@@ -9,7 +8,7 @@ var tomId = Meteor.users.insert({
     profile: { name: 'Sacha Greif' }
   });
   var sacha = Meteor.users.findOne(sachaId);
-
+  
   var telescopeId = Posts.insert({
     title: 'Introducing Telescope',
     userId: sacha._id,
@@ -18,7 +17,7 @@ var tomId = Meteor.users.insert({
     submitted: now - 7 * 3600 * 1000,
     commentsCount: 2
   });
-
+  
   Comments.insert({
     postId: telescopeId,
     userId: tom._id,
@@ -26,7 +25,7 @@ var tomId = Meteor.users.insert({
     submitted: now - 5 * 3600 * 1000,
     body: 'Interesting project Sacha, can I get involved?'
   });
-
+  
   Comments.insert({
     postId: telescopeId,
     userId: sacha._id,
@@ -34,7 +33,7 @@ var tomId = Meteor.users.insert({
     submitted: now - 3 * 3600 * 1000,
     body: 'You sure can Tom!'
   });
-
+  
   Posts.insert({
     title: 'Meteor',
     userId: tom._id,
@@ -43,7 +42,7 @@ var tomId = Meteor.users.insert({
     submitted: now - 10 * 3600 * 1000,
     commentsCount: 0
   });
-
+  
   Posts.insert({
     title: 'The Meteor Book',
     userId: tom._id,
@@ -52,4 +51,16 @@ var tomId = Meteor.users.insert({
     submitted: now - 12 * 3600 * 1000,
     commentsCount: 0
   });
+  
+  for (var i = 0; i < 10; i++) {
+    Posts.insert({
+      title: 'Test post #' + i,
+      author: sacha.profile.name,
+      userId: sacha._id,
+      url: 'http://google.com/?q=test-' + i,
+      submitted: now - i * 3600 * 1000 + 1,
+      commentsCount: 0
+    });
+  }
 }
+
